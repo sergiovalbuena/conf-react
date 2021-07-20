c4d56e1ab6d2710f629b7f676fb9a20d89041a0a
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-07-19 20:50:43 -0500

    un poco mas de css y de iconos

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Information.css';
 
 const Information = () => {
@@ -24,7 +25,9 @@ const Information = () => {
                         Regresar
                     </div>
                     <div className="Information-next">
-                        Pagar
+                        <Link to="/checkout/payment">
+                            Pagar
+                        </Link>
                     </div>
                 </div>
             </div>
