062cee7306676e85db203dadab1737330c90e9b5
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-07-23 10:46:20 -0500

    PayPal React Button added

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,13 +1,28 @@
 import React, {useRef, useContext} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 import '../styles/components/Information.css';
 
-const Information = () => {
-    const {state, addToBuyer} = useContext(ApContext);
+const Information = ({}) => {
+    const {state, addToBuyer} = useContext(AppContext);
     const form = useRef(null);
-
+    const history = useHistory();
     const {cart} = state;
+
+    const handleSubmit = () => {
+        const formData = new FormData(form.current);
+        const buyer = {
+            'name': formData.get('name'),
+            'email': formData.get('email'),
+            'address': formData.get('address'),
+            'apto': formData.get('apto'),
+            'city': formData.get('city'),
+            'country': formData.get('country'),
+            'phone': formData.get('phone'), 
+        }
+        addToBuyer(buyer);
+        history.push('/checkout/payment');
+    }
 
     return (
         <div className="Information">
@@ -33,20 +48,21 @@ const Information = () => {
                         </Link>
                     </div>
                     <div className="Information-next">
-                        <Link to="/checkout/payment">
-                            Pagar
-                        </Link>
+                        <button type="button" onClick={handleSubmit} >Pagar</button>
                     </div>
                 </div>
             </div>
             <div className="Information-sidebar">
                 <h3>Pedido:</h3>
-                <div className="Information-item">
+                {cart.map((item) =>(
+                    <div className="Information-item" key={item.title}>
                     <div className="Information-element">
-                        <h4>ITEM Name</h4>
-                        <span>$10</span>
+                        <h4>{item.title}</h4>
+                        <span>${item.price}</span>
                     </div>
-                </div>
+                    </div>
+                ))}
+
             </div>
         </div>
     );
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { PayPalButton } from "react-paypal-button-v2";
+import AppContext from '../context/AppContext';
 import '../styles/components/Payment.css'
 
-const Payment = () => {
+const Payment = ({history}) => {
+    const {state, addNewOrder} = useContext(AppContext);
+    const {cart, buyer}= state;
+
+    const paypalOptions = {
+        clientId: 'access_token$sandbox$s5p9qzp52cwvss65$262bed27704e8ad28708eb51068720f0',
+        intent: 'capture',
+        currency: 'USD'
+    }
+    const buttonStyles = {
+        layout: 'vertical',
+        shape: 'rect'
+
+    }
+
+    const handlePaymentSuccess = (data) => {
+        console.log(data);
+        if(data.status === 'COMPLETED'){
+            const newOrder = {
+                buyer, 
+                product: cart, 
+                payment: data
+            }
+            addNewOrder(newOrder)
+            history.push('/checkout/success')
+        }
+    }
+    
+    const handleSumTotal = () => {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+        const sum = cart.reduce(reducer, 0);
+        return sum;
+    };
+
+
     return (
         <div className="Payment">
             <div className="Payment-content">
                 <h3>Resumen del pedido:</h3>
+                {cart.map((item)=>(
+                    <div className="Payment-item" key={item.title}>
+                    <div className="Payment-element">
+                        <h4>{item.title}</h4>
+                        <span>$ {item.price}</span>
+                    </div>
+
+                    </div>
+                ))}
                 <div className="Payment-button">
-                    Boton de Pago PayPal
+                    <PayPalButton 
+                        paypalOptions={paypalOptions}
+                        buttonStyles={buttonStyles}
+                        amount={handleSumTotal()}
+                        onPaymentStart={() => console.log('Start Payment')}
+                        onPaymentSuccess={data => console.log(data => handlePaymentSuccess(data))}
+                        onPaymentError={error=>console.log(error)}
+                        onPaymentCancel={data => console.log(data)}
+
+                    />
                 </div>
             </div>
             <div className="sidebar"></div>
